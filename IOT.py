7fc5d362914ea87901d62b7efdf7f596a2998f9e
Prithvi <63956587+24-km@users.noreply.github.com>
2023-11-22 14:53:17 +0530

    Update IOT.py

--- IOT.py
+++ IOT.py
@@ -15,36 +15,31 @@ while True:
         print("Error: Could not read a frame.")
         break
 
-    ##### SENSOR 1 #####
+    # Define parameters for the sensors in the top row
     x1, y1, w1, h1 = 50, 50, 150, 200
-    cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
-    text = "Sensor 1 : 234 C"
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame, text, (x1, y1 - 10), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-
-    ##### SENSOR 2 #####
     x2, y2, w2, h2 = 250, 50, 150, 200
-    cv2.rectangle(frame, (x2, y2), (x2 + w2, y2 + h2), (255, 0, 0), 2)
-    text = "Sensor 2 : 234 C"
-    cv2.putText(frame, text, (x2, y2 - 10), font, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
-
-    ##### SENSOR 3 #####
     x3, y3, w3, h3 = 450, 50, 150, 200
+
+    # Define parameters for the sensors in the bottom row
+    x4, y4, w4, h4 = 50, 300, 150, 200
+    x5, y5, w5, h5 = 250, 300, 150, 200
+
+    # Draw rectangles and text for sensors in the top row
+    cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
+    cv2.rectangle(frame, (x2, y2), (x2 + w2, y2 + h2), (255, 0, 0), 2)
     cv2.rectangle(frame, (x3, y3), (x3 + w3, y3 + h3), (0, 0, 255), 2)
-    text = "Sensor 3 : 234 C"
-    cv2.putText(frame, text, (x3, y3 - 10), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
 
-    ##### SENSOR 4 #####
-    x4, y4, w4, h4 = 650, 50, 150, 200
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, "Sensor 1 : 234 C", (x1, y1 - 10), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, "Sensor 2 : 234 C", (x2, y2 - 10), font, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, "Sensor 3 : 234 C", (x3, y3 - 10), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
+
+    # Draw rectangles and text for sensors in the bottom row
     cv2.rectangle(frame, (x4, y4), (x4 + w4, y4 + h4), (255, 255, 0), 2)
-    text = "Sensor 4 : 234 C"
-    cv2.putText(frame, text, (x4, y4 - 10), font, 0.8, (255, 255, 0), 2, cv2.LINE_AA)
-
-    ##### SENSOR 5 #####
-    x5, y5, w5, h5 = 850, 50, 150, 200
     cv2.rectangle(frame, (x5, y5), (x5 + w5, y5 + h5), (255, 0, 255), 2)
-    text = "Sensor 5 : 234 C"
-    cv2.putText(frame, text, (x5, y5 - 10), font, 0.8, (255, 0, 255), 2, cv2.LINE_AA)
+
+    cv2.putText(frame, "Sensor 4 : 234 C", (x4, y4 - 10), font, 0.8, (255, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, "Sensor 5 : 234 C", (x5, y5 - 10), font, 0.8, (255, 0, 255), 2, cv2.LINE_AA)
 
     cv2.imshow('Webcam', frame)
 
